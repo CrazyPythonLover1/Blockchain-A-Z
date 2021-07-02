aea3e00eb4cded6e1ea8fd4fb6af7b704ed62224
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-02 17:20:44 +0600

    create_block, get_prev_block

--- 1. Module - Create Blockchain/blockchain.py
+++ 1. Module - Create Blockchain/blockchain.py
@@ -1,8 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  2 16:10:59 2021
+# Module 1 -- Create a Blockchain 
 
-@author: mif
-"""
+# Flask -- pip install Flask==0.12.2
+# Postman
 
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 -- Building a blockchain
+
+class Blockchain: 
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+    def create_block(self, proof, previous_hash):
+        block = {
+                'index': len(self.chain) + 1,
+                'timestamp': str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash': previous_hash,
+                'data': ''
+            }
+        self.chain.append(block)
+        return block
+        
+    def get_previous_block(self):
+        return self.chain[-1]
