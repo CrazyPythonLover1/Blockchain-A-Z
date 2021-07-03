c0c52023bb4047627823811b12a23f6a48e4bc6c
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-04 00:17:47 +0600

    mine a block and get req with Flask

--- 1. Module - Create Blockchain/blockchain.py
+++ 1. Module - Create Blockchain/blockchain.py
@@ -23,7 +23,6 @@ class Blockchain:
                 'timestamp': str(datetime.datetime.now()),
                 'proof': proof,
                 'previous_hash': previous_hash,
-                'data': ''
             }
         self.chain.append(block)
         return block
@@ -61,3 +60,54 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+    
+# Part 2 -- Mining Our Blockchain
+
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a Blockchain Object
+blockchain = Blockchain()
+
+# mining a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
