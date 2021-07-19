3205779dcf350f05616360e375fb51e27ecab3a2
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-19 19:33:22 +0600

    step 6 for creating cryptocurrency

--- 2. Module - create Cryptocurrency/hadcoin.py
+++ 2. Module - create Cryptocurrency/hadcoin.py
@@ -105,6 +105,9 @@ class Blockchain:
 app = Flask(__name__)
 #app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-','')
+
 # Creating a Blockchain Object
 blockchain = Blockchain()
 
@@ -115,12 +118,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver = 'Hadelin', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash']
+                'transactions': block['transactions']}
     return jsonify(response), 200
     
 # Getting the full Blockchain
@@ -140,6 +145,19 @@ def is_valid():
         response = {'message': 'Houston, we have a problem. The Blockchain is no valid'}
     return jsonify(response), 200
 
+# Adding a new transaction to the blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return "Some elements of the transaction are missing", 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {
+        'message': f'This transaction will be added to Block {index}'
+    }
+    return jsonify(response), 201
+    
 # Part 3 - Decentralizing our Blockchain
 
 # Running the app
